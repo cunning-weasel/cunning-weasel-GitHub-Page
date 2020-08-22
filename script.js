5796ff929789925d888faf0d02fdf8aec2cdbda1
Paul <you@example.com>
2020-08-22 22:09:47 +0200

    refactored containers with flex1

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
 // add sexy show and hide on.click functionality to navbar menu
 
 // ditto for projects navigation
 
+// later add projects i haven't thought of yet that do some basic js logical stuff
 
