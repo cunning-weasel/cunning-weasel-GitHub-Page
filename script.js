9c60129f0083e1647b112a09d6ef7a35b29ff3f7
Paul <you@example.com>
2020-08-31 08:57:05 +0200

    added nav functionality and small changes

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// add sexy show and hide navbar menu with click functionality 
+/* 
 
-// ditto for projects navigation
-    // update: added inline js for click functionality
+When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 
-// later add projects i add
+onscroll event executes js when div elem. tag is being scrolled. in markup, needs to be placed in elem as attri and reference
+the script
+
+in js, object needs to tie to onscroll event, with a func that takes a script
+
+pageYOffset returns the pixels in the durrent doc has been scrolled horiznontally or vertically (pageXOffset)
+
+*/
+
+let prevScrollpos = window.pageYOffset; 
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset; 
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px"; // double check why navbar doesn't auto-hide
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
 
