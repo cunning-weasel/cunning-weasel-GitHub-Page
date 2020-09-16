ada315cdcf7240c3fea845b1e3ea8032bbb48ad6
Paul <you@example.com>
2020-09-16 22:11:01 +0200

    resolved API fetch and added loop for added prettyness

--- script.js
+++ script.js
@@ -1,34 +1,78 @@
-/* 
-After the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
-*/
 "use strict";
 
-let prevPos = window.pageYOffset; // declare variable to store scroll positon globally
+// pretty navbar
+let prevPos = window.pageYOffset;
+// console.log(prevPos + " previous Global");
 window.onscroll = function () {
-  let currentPos = window.pageYOffset; // pageYOffset returns the pixels in the current doc scrolled vertically
-  // window.onscroll = function () {console.log (1) }; confirm event is firing
+  let currentPos = window.pageYOffset;
+  // window.onscroll = function () {console.log (1) }; 
+  // console.log(currentPos + " current Pos");
+  // console.log(prevPos + " previous Pos");            - need another debugging method tbh
   if (prevPos > currentPos) {
-    // if previous scroll position is more than where the user currently is, return fully visible nav
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px"; // otherwise remove visibility by 50px
+    document.getElementById("navbar").style.top = "-50px";
   }
-  prevPos = currentPos; // set previous scroll position to current to call the funuction again 
+  prevPos = currentPos;
 }
 
-// get repos in rss feed/ style?
-// could store all projects, but need to find how to get them - and how to store them
+// start GH API projects-page integration
 
-/*
+const req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/cunning-weasel/repos", true);
+req.send();
+req.onload = function () {
+    const json = JSON.parse(req.responseText);
+    let html = "";
+    json.forEach(function (val) {
+      const keys = Object.keys(val);
+      html += "<div id = 'test'>";
+      keys.forEach(function (key) {
+        html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
+      });
+      html += "</div><br>";
+    });
+    document.getElementById("projects-sub").innerHTML = html;
+  };
 
-let allRepos = [];
 
-allRepos.push({
-  name: "name",
-  description: description,
-  ...
-})
+    // fetch("https://api.github.com/users/cunning-weasel/repos")
+    //   .then(response => response.json()) // convert response to json
+    //   .then(data => { // json object i need!
+    //     document.getElementById("projects-sub").innerHTML = JSON.stringify(data); // textContent instead?
+    //   })
 
-*/
+    /*
+    const req = new XMLHttpRequest();
+    req.open("GET","https://api.github.com/users/cunning-weasel/repos",true);
+    req.send();
+    req.onload = function() {
+      const json = JSON.parse(req.responseText);
+      document.getElementById("projects-sub").innerHTML = JSON.stringify(json);
+    };
+
+    https://api.github.com/users/cunning-weasel/repos
+
+    GH REST API:
+    no need for token (thank gawd)
+
+    Repos end-point:
+    api.github.com/orgs/users/cunning-weasel/repos || https://api.github.com/users/cunning-weasel/repos
+    get JSON to send response to GH API. Once data is there, can loop api.jquery.com
+    play around, since I need li item, then I can get a link:
+    https://api.github.com/users/cunning/repos
+    https://api.jquery.com/jQuery.getJSON/ 
 
 
+    let allRepos = [];
+
+    allRepos.push({
+      name: "name",
+      description: description,
+      ...
+    })
+    */
+
+    // start GH API projects-page integration
+
+    // start data-visualizations to segment lang, location 
