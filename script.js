4f7cef664014be895d31cd512b9833af40a736d7
weasel <paulbongers22@gmail.com>
2023-09-26 21:47:43 +0200

    serve on ghp for now... c's a problem!

--- script.js
+++ script.js
@@ -1,88 +1,77 @@
 "use strict";
+// change up for server endpoints
+document.addEventListener("DOMContentLoaded", () => {
+  const searchInput = document.querySelector(".searchBox");
+  searchInput.disabled = true;
+  let repoList = document.querySelector("#repos");
+  let repos = [];
 
-// pretty navbar
-let prevPos = window.pageYOffset;
-window.onscroll = () => {
-  let currentPos = window.pageYOffset;
-  // console.log(prevPos + " previous Pos");
-  if (prevPos > currentPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-  prevPos = currentPos;
-};
-
-// darkmode
-const dark_light = () => {
-  const button = document.querySelector(".dark-light-button");
-  const docBod = document.body;
-
-  button.addEventListener("click", (event) => {
-    const mode = docBod.classList.contains("dark-mode") ? "light" : "dark";
-    button.textContent = `${mode} mode`;
-    docBod.classList.toggle("dark-mode");
-    console.log("dark-light triggering master weasel");
+  searchInput.addEventListener("keyup", (e) => {
+    const searchStr = e.target.value.toLowerCase();
+    const filteredRepos = repos.filter((repo) => {
+      return (
+        repo.name.toLowerCase().includes(searchStr) ||
+        (repo.language && repo.language.toLowerCase().includes(searchStr)) ||
+        (repo.description && repo.description.toLowerCase().includes(searchStr))
+      );
+    });
+    showRepos(filteredRepos);
   });
-};
 
-// start search input/ api render
-const searchInput = document.querySelector(".searchBox");
-searchInput.disabled = true; // disable search input
-let repoList = document.querySelector("#projects-sub");
-let repos = [];
+  const pullRepos = async () => {
+    try {
+      const api_url = "https://api.github.com/users/cunning-weasel/repos";
+      const res = await fetch(api_url);
+      repos = await res.json();
+      showRepos(repos);
+      searchInput.disabled = false;
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-// search logic
-searchInput.addEventListener("keyup", (e) => {
-  const searchStr = e.target.value.toLowerCase();
-
-  const filteredRepos = repos.filter((repo) => {
-    return (
-      repo.name.toLowerCase().includes(searchStr) ||
-      (repo.language && repo.language.toLowerCase().includes(searchStr)) ||
-      (repo.description && repo.description.toLowerCase().includes(searchStr))
-    );
-  });
-  shoRepos(filteredRepos);
+  const showRepos = (repos) => {
+    const htmlString = repos
+      .map((repo) => {
+        return `
+        <div class="card">
+            <div class="card-container">
+                <div>
+                    <p>${repo.language}</p>
+                </div>
+                <h2>${repo.name}</h2>
+            </div>
+                <div>
+                    <p>${repo.description}</p>
+                </div>
+                <div>
+                    <a href="${repo.homepage}" class="btn">Read more</a>
+                </div>
+        </div>
+          `;
+      })
+      .join("");
+    repoList.innerHTML = htmlString;
+  };
+  pullRepos();
 });
 
-// api call
-const pullRepos = async () => {
-  try {
-    const api_url = "https://api.github.com/users/cunning-weasel/repos";
-    const res = await fetch(api_url);
-    repos = await res.json();
-    console.log(repos);
-    shoRepos(repos);
-    searchInput.disabled = false; // enable search input
-  } catch (err) {
-    console.error(err);
-  }
+// needs to be publish date - some static var
+const currentDate = new Date(Date.now());
+
+const dateFormat = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
 };
 
-const shoRepos = (repos) => {
-  const htmlString = repos
-    .map((repo) => {
-      return `
-    <div class="card">
-    <div class="card-container">
-      <h2> ${repo.name} </h2>
-    </div>
-    <div>
-      <p>${repo.language}</p>
-    </div>
-    <div>
-    <p>${repo.description}</p>
-  </div>
-    <div>
-      <a href="${repo.html_url}" target="_blank">Repository</a>
-      <a href="${repo.homepage}" target="_blank">Website</a>
-    </div>
-</div>
-    `;
-    })
-    .join("");
-  repoList.innerHTML = htmlString;
-};
+const formattedDate = new Intl.DateTimeFormat("en-US", dateFormat).format(
+  currentDate
+);
 
-pullRepos();
+document.getElementById("currentDate").textContent = formattedDate;
+
+// pass endpoints from above
+document.getElementById("content").innerHTML = marked.parse(
+  "# Article title goes here\n\nRest of the text and some **bold** sample all written in .md and rendered to html with marked."
+);
