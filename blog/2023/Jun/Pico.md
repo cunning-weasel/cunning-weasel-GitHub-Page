f240c1ee67bdd5de3fe97f9e6d4c1dc8b4de41af
weasel <paulbongers22@gmail.com>
2023-10-12 22:46:28 +0200

    FE only sucks

---
+++ blog/2023/Jun/Pico.md
@@ -0,0 +1,9 @@
+## To-Do's
+
+### ESP8266 12E D1 Mini Responsibilities:
+
+- **Configure as an Access Point (AP):** The ESP8266 should create a local Wi-Fi network.
+- **Implement a WebSockets Server:** Run a WebSockets server to handle game communication between clients.
+- **Manage LAN Connections:** Handle LAN connections and coordinate multiplayer games at a high level.
+
+I want to dynamically pick up the date, heading and the fist few sentences of each article, and of course the article link as already provided
--- script.js
+++ script.js
@@ -1,5 +1,138 @@
 "use strict";
-// change up for server endpoints
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const searchInput = document.querySelector(".searchBox");
+  let articleList = document.querySelector("#blogArticles");
+  let blogArticleContent = document.querySelector("#blogArticleContent");
+
+  const articlePaths = [
+    "/blog/2023/Sep/Growth.md",
+    "/blog/2023/Jun/Pico.md",
+    "/blog/2023/Aug/Anime.md",
+    // more paths...
+  ];
+
+  const monthsToRetrieve = ["Sep", "Jun", "Aug"];
+
+  const fetchMarkdownFile = async (articlePath) => {
+    searchInput.disabled = false;
+
+    try {
+      const resp = await fetch(articlePath);
+
+      if (resp.ok) {
+        const markdownText = await resp.text();
+        console.log("markdownText fetch:", markdownText);
+        return markdownText;
+      } else {
+        throw new Error("Failed to fetch .md file");
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const showArticles = async (articlePaths) => {
+    const htmlContent = await Promise.all(
+      articlePaths
+        .filter((path) => {
+          const month = path.split("/")[3];
+          return monthsToRetrieve.includes(month);
+        })
+        .map(async (path) => {
+          const markdownText = await fetchMarkdownFile(path);
+          const { date, title, content } = parseMarkdown(markdownText);
+          return `
+            <div class="card">
+              <div class="card-container">
+                <div>
+                  <p>${date}</p>
+                </div>
+                <h2>${title}</h2>
+              </div>
+              <div>
+                <p>${content}</p>
+              </div>
+              <div>
+                <a href="${path}" class="btn" id="articleLink">Read more</a>
+              </div>
+            </div>
+          `;
+        })
+    );
+
+    articleList.innerHTML = htmlContent.join("");
+  };
+
+  const parseMarkdown = (markdownText) => {
+    const date = "Your Date Logic";
+    const title = "Your Title Logic";
+    const content = "Your Content Logic";
+    return { date, title, content };
+  };
+
+  const readMoreLinks = articleList.querySelectorAll("#articleLink");
+  readMoreLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const articleId = e.currentTarget.getAttribute("href").substring(1);
+      console.log("articleId", articleId);
+      showSingleArticle(articleId);
+    });
+  });
+
+  const showSingleArticle = async (articleId) => {
+    const articlePath = `/blog/2023/${articleId}.md`;
+    const markdownText = await fetchMarkdownFile(articlePath);
+    console.log("article paths:", markdownText);
+    const htmlContent = marked.parse(markdownText);
+    blogArticleContent.innerHTML = htmlContent;
+  };
+
+  searchInput.addEventListener("keyup", (e) => {
+    const searchStr = e.target.value.toLowerCase();
+    const filteredArticles = articles.filter((article) => {
+      const title = article.title.toLowerCase();
+      const content = article.content.toLowerCase();
+      return title.includes(searchStr) || content.includes(searchStr);
+    });
+    showArticles(filteredArticles);
+  });
+
+  // initial render
+  showArticles(articlePaths);
+});
+
+
+// needs to be publish date - some static var
+// const currentDate = new Date(Date.now());
+
+// const dateFormat = {
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+// };
+
+// const formattedDate = new Intl.DateTimeFormat("en-US", dateFormat).format(
+//   currentDate
+// );
+
+// document.getElementById("currentDate").textContent = formattedDate;
+
+
+
+
+
+
+
+
+
+/*
+
+old site
+
+
 // document.addEventListener("DOMContentLoaded", () => {
 //   const searchInput = document.querySelector(".searchBox");
 //   searchInput.disabled = true;
@@ -55,71 +188,4 @@
 //   };
 //   pullRepos();
 // });
-
-// begin file read ish
-async function fetchMarkdownFile(filePath) {
-  const resp = await fetch(filePath);
-  if (resp.ok) {
-    const markdownText = await resp.text();
-    return markdownText;
-  } else {
-    throw new Error("Failed to fetch .md file");
-  }
-}
-
-const markdownText = await fetchMarkdownFile("your-article.md");
-const htmlContent = marked(markdownText);
-
-const blogArticlesContainer = document.getElementById("blogArticles");
-
-// assuming an arr of article objects
-const articles = [
-  {
-    title: "Article Title 1",
-    content: "Markdown content for article 1...",
-  },
-  {
-    title: "Article Title 2",
-    content: "Markdown content for article 2...",
-  },
-  // more articles here
-];
-
-articles.forEach((article, index) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  card.innerHTML = `
-  <div>${article.date}</div>
-    <h2>${article.title}</h2>
-    <div>${article.content}</div>
-    <a href="blogArticle.html#${index}" class="btn">Read more</a>
-  `;
-
-  blogArticlesContainer.appendChild(card);
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const articleIndex = window.location.hash.replace("#", "");
-  // retrieve the article data by index and render it here.
-});
-
-// pass endpoints from above i.e
-// document.getElementById("content").innerHTML = marked.parse(
-//   "# Article title goes here\n\nRest of the text and some **bold** sample all written in .md and rendered to html with marked."
-// );
-
-// needs to be publish date - some static var
-const currentDate = new Date(Date.now());
-
-const dateFormat = {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-};
-
-const formattedDate = new Intl.DateTimeFormat("en-US", dateFormat).format(
-  currentDate
-);
-
-document.getElementById("currentDate").textContent = formattedDate;
+*/
